7f259ae960eb24445f0f32acbe0e382c7b0b6798
= <=>
2023-10-09 15:28:41 +0700

    Update | Pertemuan 7

---
+++ week07/src/layout_flutter/README.md
@@ -0,0 +1,16 @@
+### Nama    : Devi Andini Febrianti
+### No/Nim  : 09/2141720189
+### Kelas   : D4TI-3H
+### Jurusan : Teknologi Informasi
+
+#### Praktikum 1: Membangun Layout di Flutter
+![Getting Started](images/01.png)
+
+#### Praktikum 2: Implementasi button row
+![Getting Started](images/02.png)
+
+#### Praktikum 3: Implementasi text section
+![Getting Started](images/03.png)
+
+#### Praktikum 4: Implementasi image section
+![Getting Started](images/04.png)
---
+++ week07/src/layout_flutter/lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 25),
+                  child: const Text(
+                    'Kediri Lagi',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Merupakan kota yang memiliki beberapa wisata.',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+    Color color = Theme.of(context).primaryColor;
+
+      Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+        'Kadiri maupun Kediri sama-sama berasal dari bahasa Sansekerta, dalam etimologi "Kadiri" disebut sebagai "Kedi" yang artinya "Mandul", tidak berdatang bulan (aprodit). Dalam bahasa Jawa Kuno, "Kedi" juga mempunyai arti "Dikebiri" atau dukun.',
+        softWrap: true,
+      ),
+    );
+
+    return MaterialApp(
+      title: 'Devi Andini Febrianti - 2141720189',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: ListView(
+          children: [
+            Image.asset(
+              'images/kediri_lagi.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
