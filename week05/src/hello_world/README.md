4ff5c219e8185ea78c3cbfa8ac6eb565d3699e85
= <=>
2023-10-10 08:05:36 +0700

    Upload namer_app

--- week05/src/hello_world/README.md
+++ week05/src/hello_world/README.md
@@ -384,3 +384,13 @@ Dan ini hasil setelah mengganti tanggal
 ![Getting Started](images/11.png)
 
 ------
+ ### Tugas Praktikum
+1. Selesaikan Praktikum 1 sampai 4, lalu dokumentasikan dan push ke repository Anda berupa screenshot setiap hasil pekerjaan beserta penjelasannya di file README.md!
+
+2. Pada praktikum 4 mulai dari Langkah 3 sampai 6, buatlah file widget tersendiri di folder basic_widgets, kemudian pada file main.dart cukup melakukan import widget sesuai masing-masing langkah tersebut!
+
+3. Selesaikan Codelabs: Your first Flutter app, lalu buatlah laporan praktikumnya dan push ke repository GitHub Anda!
+
+4. README.md berisi: capture hasil akhir tiap praktikum (side-by-side, bisa juga berupa file GIF agar terlihat proses perubahan ketika ada aksi dari pengguna) di browser dan perangkat fisik (device) dengan menampilkan NIM dan Nama Anda sebagai ciri pekerjaan Anda. Jika mode developer di perangkat HP Anda belum aktif, silakan cari di internet cara mengaktifkannya!
+
+5. Kumpulkan berupa link repository/commit GitHub Anda ke tautan spreadsheet yang telah disepakati oleh dosen!
--- week05/src/hello_world/lib/main.dart
+++ week05/src/hello_world/lib/main.dart
@@ -1,5 +1,5 @@
 // import 'package:flutter/material.dart';
-// import 'basic_widgets/text_widget.dart';
+// // import 'basic_widgets/text_widget.dart';
 // import 'basic_widgets/image_widget.dart';
 
 // void main() {
@@ -107,7 +107,7 @@
 //           // wireframe for each widget.
 //           mainAxisAlignment: MainAxisAlignment.center,
 //           children: <Widget>[
-//             const MyTextWidget(),
+//             const MyImageWidget(),
 //             Text(
 //               '$_counter',
 //               style: Theme.of(context).textTheme.headlineMedium,
@@ -241,74 +241,99 @@
 //   }
 // }
 
-import 'dart:async';
+// import 'dart:async';
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(const MyApp());
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       title: 'Contoh Date Picker',
+//       home: MyHomePage(title: 'Contoh Date Picker'),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+//   final String title;
+
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   // Variable/State untuk mengambil tanggal
+//   DateTime selectedDate = DateTime.now();
+
+//   //  Initial SelectDate FLutter
+//   Future<void> _selectDate(BuildContext context) async {
+//     // Initial DateTime FIinal Picked
+//     final DateTime? picked = await showDatePicker(
+//         context: context,
+//         initialDate: selectedDate,
+//         firstDate: DateTime(2015, 8),
+//         lastDate: DateTime(2101));
+//     if (picked != null && picked != selectedDate) {
+//       setState(() {
+//         selectedDate = picked;
+//       });
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisSize: MainAxisSize.min,
+//           children: <Widget>[
+//             Text("${selectedDate.toLocal()}".split(' ')[0]),
+//             const SizedBox(
+//               height: 20.0,
+//             ),
+//             ElevatedButton(
+//               onPressed: () => {
+//                 _selectDate(context),
+//                 // ignore: avoid_print
+//                 print(selectedDate.day + selectedDate.month + selectedDate.year)
+//               },
+//               child: const Text('Pilih Tanggal'),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
-
+void main(){
+  runApp(const MyApp());
+}
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Contoh Date Picker',
-      home: MyHomePage(title: 'Contoh Date Picker'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // Variable/State untuk mengambil tanggal
-  DateTime selectedDate = DateTime.now();
-
-  //  Initial SelectDate FLutter
-  Future<void> _selectDate(BuildContext context) async {
-    // Initial DateTime FIinal Picked
-    final DateTime? picked = await showDatePicker(
-        context: context,
-        initialDate: selectedDate,
-        firstDate: DateTime(2015, 8),
-        lastDate: DateTime(2101));
-    if (picked != null && picked != selectedDate) {
-      setState(() {
-        selectedDate = picked;
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text("${selectedDate.toLocal()}".split(' ')[0]),
-            const SizedBox(
-              height: 20.0,
-            ),
-            ElevatedButton(
-              onPressed: () => {
-                _selectDate(context),
-                // ignore: avoid_print
-                print(selectedDate.day + selectedDate.month + selectedDate.year)
-              },
-              child: const Text('Pilih Tanggal'),
-            ),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Contoh TextField")),
+        body: const TextField(
+          obscureText: false,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Nama',
+          ),
         ),
       ),
     );
