edcf57cb76358f3901fca61f658a193f33bc9635
= <=>
2023-11-11 14:42:02 +0700

    praktikum 1-2

---
+++ week11/src/master_plan/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import './models/plan.dart';
+import './views/plan_screen.dart';
+import './provider/plan_provider.dart';
+
+void main() => runApp(MasterPlanApp());
+
+class MasterPlanApp extends StatelessWidget {
+  const MasterPlanApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+  theme: ThemeData(primarySwatch: Colors.purple),
+  home: PlanProvider(
+    notifier: ValueNotifier<Plan>(const Plan()),
+    child: const PlanScreen(),
+  ),
+);
+
+    // return MaterialApp(
+    // theme: ThemeData(primarySwatch: Colors.purple),
+    // home: PlanScreen(),
+    // );
+  }
+}
