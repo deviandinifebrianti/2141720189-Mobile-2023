29d36c0a4b468c4eb9303bab7fd55f99f58d21d3
= <=>
2023-11-12 20:41:02 +0700

    prak 3

--- week11/src/master_plan/lib/main.dart
+++ week11/src/master_plan/lib/main.dart
@@ -1,26 +1,38 @@
 import 'package:flutter/material.dart';
-import './models/plan.dart';
-import './views/plan_screen.dart';
-import './provider/plan_provider.dart';
+import 'models/plan.dart';
+import 'provider/plan_provider.dart';
+import 'views/plan_creator_screen.dart';
 
-void main() => runApp(MasterPlanApp());
+void main() => runApp(const MasterPlanApp());
 
 class MasterPlanApp extends StatelessWidget {
   const MasterPlanApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-  theme: ThemeData(primarySwatch: Colors.purple),
-  home: PlanProvider(
-    notifier: ValueNotifier<Plan>(const Plan()),
-    child: const PlanScreen(),
-  ),
-);
+    return PlanProvider(
+      notifier: ValueNotifier<List<Plan>>(const []),
+      child: MaterialApp(
+        title: 'State management app',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const PlanCreatorScreen(plan: Plan(name: 'My Plan EveryDay')),
+      ),
+    );
+  }
+}
+
+    
+  //   return MaterialApp(
+  //   theme: ThemeData(primarySwatch: Colors.purple),
+  //   home: PlanProvider(
+  //     notifier: ValueNotifier<Plan>(const Plan()),
+  //     child: const PlanScreen(),
+  //   ),
+  // );
 
     // return MaterialApp(
     // theme: ThemeData(primarySwatch: Colors.purple),
     // home: PlanScreen(),
     // );
-  }
-}
